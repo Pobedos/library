4388ed2816d643c24d05e44a7b797631218cdc2d
ArtyomScheredin <mrboxman75@gmail.com>
2022-04-21 00:52:58 +0300

    Rework

--- src/test/java/ru/pobedonostsev/library/LibraryApplicationTests.java
+++ src/test/java/ru/pobedonostsev/library/LibraryApplicationTests.java
@@ -1,5 +1,6 @@
 package ru.pobedonostsev.library;
 
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -8,6 +9,7 @@ class LibraryApplicationTests {
 
     @Test
     void contextLoads() {
+        Assertions.assertEquals(1,1);
     }
 
 }
